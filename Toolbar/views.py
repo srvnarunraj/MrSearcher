d86460e1d055daac4f9c21eec6a50aa2b31f0842
srvnn_arunraj <arunraj14092002@gmail.com>
2022-12-13 12:55:49 +0530

    Added Image Crawler also

--- Toolbar/views.py
+++ Toolbar/views.py
@@ -1,7 +1,11 @@
+import os
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from youtubesearchpython import VideosSearch
 from Toolbar.models import SearchText 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 
 def startup(request):
     return render(request,'index.html')
@@ -11,7 +15,7 @@ def search(request):
     if stxt.isspace() == False and len(stxt)!=0:
         input = SearchText(SearchText=stxt)
         input.save()
-    return render(request,'header.html')
+    return render(request,'main.html')
 
 def videos(request):
     # if request.method =='POST':
@@ -46,5 +50,42 @@ def videos(request):
     #     return redirect('/')
 
 def images(request):
-    stxt = SearchText.objects.latest('SearchText')
-    return HttpResponse(stxt)
+    searchkey = SearchText.objects.latest('SearchText')
+    searchkey = str(searchkey)
+    mypath= "E:\\chromedriver.exe"
+    myoptions = webdriver.ChromeOptions()
+    myoptions.add_experimental_option('detach',True)
+    s= Service(mypath)
+    driver = webdriver.Chrome(options=myoptions,service=s)
+    driver.get('https://images.google.com/')
+    box=driver.find_element("xpath",'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+    mykey=str(searchkey)
+    box.send_keys(mykey)
+    box.send_keys(Keys.ENTER)
+    try:
+        n=mykey
+        pd='E:/Python Search Engine/MrSearch/static/images/'
+        path = os.path.join(pd,n)
+        os.mkdir(path)
+    except:
+        pass
+    for i in range(1,21):
+        try:
+            img = driver.find_element('xpath','//*[@id="islrg"]/div[1]/div['+str(i)+']/a[1]/div[1]/img').screenshot(path+'\img'+str(i)+'.png')
+        except:
+            pass
+        searchtxt=searchkey
+        x = 'E:\Python Search Engine\MrSearch\static\images\\'
+            # searchtxt='Robot'
+        url=x+searchtxt
+        size = os.listdir(url)
+        location=[]
+        myurl='images//'+searchtxt+'//'
+        for i in size:
+            location.append(myurl+i+'')
+        mydict={
+            'size':size,
+            'locate':location   
+        }
+    return render(request,'main.html',mydict)
+        
