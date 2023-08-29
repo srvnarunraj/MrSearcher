62b0cb8f1726b7ec94e50cd6dc2ee9fba960bb89
Arun Raj <arunraj14092002@gmail.com>
2023-08-29 19:21:38 +0530

    Admin Page CSS

--- Toolbar/admin.py
+++ Toolbar/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from Toolbar.models import SearchText
 # Register your models here.
 admin.site.register(SearchText)
+
+admin.site.site_header="Mr Searcher Portal"
+admin.site.site_title="Mr Searcher Admin Login"
+admin.site.index_title="Mr Searcher"
